cb044a76a8948f3e19b12a467b13b37c5b61edfd
GauravShripad989 <gauravshripad127@gmail.com>
2023-10-06 10:47:57 +0530

    Question 3 solved

---
+++ Question_3.java
@@ -0,0 +1,10 @@
+package Chapter_2_Practice_set_2.Chapter_2_Practice_set_2;
+import java.util.Scanner;
+public class Question_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        System.out.println(num>8);
+    }
+}
