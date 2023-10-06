47d110381763be14b414cbb5b0f7c2680bacbca3
GauravShripad989 <gauravshripad127@gmail.com>
2023-10-06 10:36:37 +0530

    Question 1 solved

---
+++ Question_1.java
@@ -0,0 +1,8 @@
+package Chapter_2_Practice_set_2.Chapter_2_Practice_set_2;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        float a = 7/4 * 9/2;
+        System.out.println(a);
+    }
+}
